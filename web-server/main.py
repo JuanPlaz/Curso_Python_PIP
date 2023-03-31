a942d177be705bfe7bdc9b558ce4f39f770f2a60
JuanPlaz <juanplaz3@gmail.com>
2023-03-31 13:19:09 +1100

    Implementation of FastAPI and Uvicorn

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,21 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1, 2, 3]
+
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1> Hello There! I'm a web page! </h1>
+        <p> A beautiful paragrahp here! </p>
+    """
+
 
 def run():
     store.get_categories()
